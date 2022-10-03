e261c6c354249948bfdc3279eb280f42a9a9b6e4
Jothir Adithyan <adithyanjothir@gmail.com>
2022-10-03 17:40:28 +0530

    Added countriesQuery not paginated

--- Task2/serve.py
+++ Task2/serve.py
@@ -2,20 +2,52 @@ import falcon
 
 from falcon_graphene import GrapheneRouter
 
+from graphene import ObjectType,String,Schema,JSONString,Int
 import graphene
 
+from mongoengine import *
 
-class Clock(graphene.ObjectType):
-    name = graphene.String()
+connect('countries')
+
+class Country(Document):
+    country_id = IntField()
+    name = StringField()
+    capital = ListField()
+    area = FloatField()
+    timezone = ListField()
+    independance = BooleanField()
+    continents = ListField()
+    un_member = BooleanField()
+    languages = DictField()
+    population = IntField()
+    region = StringField()
 
 
-class RootQuery(graphene.ObjectType):
-    clock = graphene.Field(Clock)
+class Query(ObjectType):
+    countriesQuery = JSONString()
+    countryQuery = JSONString(id = Int(required=True))
+    goodbye = String()
 
-    def resolve_clock(self, args, context, info):
-        return Clock(name="Charlie")
+    def resolve_countriesQuery(root, info):
+        
+        ctry = Country.objects()
+        json_response = ctry.to_json()
+        return json_response
+
+    def resolve_countryQuery(root, info, id):
+        
+        ctry = Country.objects(country_id=id).first()
+        json_response = ctry.to_json()
+        return json_response
+
+
+
+    def resolve_goodbye(root, info):
+        return 'See ya!'
+
+schema = Schema(query=Query)
 
 
 application = falcon.API()
-schema = graphene.Schema(query=RootQuery)
+schema = graphene.Schema(query=Query)
 router = GrapheneRouter.from_schema(schema).serving_on(application)
